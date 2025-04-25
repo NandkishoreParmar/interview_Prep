b20a7280d2a84d0bd91733cf1850e424355f4a7a
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-04-25 15:37:09 +0530

    Redux Toolkit

---
+++ React/Redux-app/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './Redux/counter/CounterSlice'
+
+function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <div className="card">
+          count is {count}
+
+   </div>
+   <button onClick={()=> dispatch(decrement())}>-</button>
+
+   <button onClick={()=> dispatch(increment())}>+</button>
+    </>
+  )
+}
+
+export default App
---
+++ React/Redux-app/src/components/Navbar.jsx/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = ()=>{
+  return (
+    <>
+    <div>
+      count {count} is access by Redux store
+    </div>
+    </>
+  )
+}
+export default Navbar;
