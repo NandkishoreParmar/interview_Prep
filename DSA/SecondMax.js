1322db317ac196f10f72ae41c4747cebbfc3fb56
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-05-07 18:04:15 +0530

    second approach

--- DSA/SecondMax.js
+++ DSA/SecondMax.js
@@ -23,3 +23,22 @@ console.log(secondHighest("dfa12321afd")); // Output: 2
 console.log(secondHighest("abc1111"));     // Output: -1
 console.log(secondHighest("87329"));       // Output: 8
 console.log(secondHighest("abc"));
+
+
+
+function second(arr){
+    let firstlargest = arr[0]
+    let secondlargest = arr[0]
+
+    for (let i=0; i<arr.length; i++){
+        if (arr[i]>firstlargest){
+            secondlargest = firstlargest
+            firstlargest = arr[i]
+        }else if( arr[i]>secondlargest){
+            secondlargest= arr[i]
+        }
+    }
+    return secondlargest
+}
+let arr = [1,2,3,4,5,7,8]
+console.log("second",second(arr)); 
