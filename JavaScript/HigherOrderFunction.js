edabff88e1cca26b073f6fcaa2b14403f564366f
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-05-28 18:10:05 +0530

    Higher Order

---
+++ JavaScript/HigherOrderFunction.js
@@ -0,0 +1,47 @@
+
+
+// forEach return undefined
+let arr = [1,2,3,4,5,6];
+
+let narr = arr.forEach((el,i,arr)=>
+  console.log(el,i,arr)
+)
+
+console.log("+++++++++++++")
+//map
+// map create a duplicate copy it doessnot change the original array
+let maparr = arr.map((el,i,arr)=>{
+  return el*2    // el % 2==0   
+})
+console.log(maparr)
+console.log(arr)
+
+// filter 
+let fruits = ["Apple","Banana","Grapes","Papaya"];
+
+function searchFruits(fruit){
+  let fruit1 = fruits.filter((el,i,arr)=>{
+    return el == fruit
+  
+  })
+  // console.log(fruit1)
+  if (fruit1.length == 0){
+    console.log("Not found")
+  }else{
+    console.log(fruit1)
+  }
+
+}
+searchFruits("Apples")
+
+//reduce
+console.log("Reduceeeeeeeeeeeeeeeeeeeee")
+let newArr = [2,3,4];
+
+function sumofN(){
+  let sum = newArr.reduce((acc,curr)=> {
+    return acc = acc + curr
+  },10)
+  console.log(sum);
+}
+sumofN();
