17093f185aab3ce8364a136f8ceb9c3c8d2ab633
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-05-30 15:34:25 +0530

    Cloure

---
+++ CurriculamAssignment/closure/closure.js
@@ -0,0 +1,10 @@
+function outerFunction() {
+  const message = "Hello from the closure!";
+  function innerFunction() {
+    console.log(message);
+  }
+  return innerFunction;
+}
+
+const myClosure = outerFunction();
+myClosure();
