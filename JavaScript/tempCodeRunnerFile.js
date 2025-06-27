2b4978cfddcb024f187231b7d02c6847d48bdd8d
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-06-27 11:00:44 +0530

    Class constructor created

--- JavaScript/tempCodeRunnerFile.js
+++ JavaScript/tempCodeRunnerFile.js
@@ -1,6 +1,15 @@
+class Student{
+  constructor(name,roll,age){
+    this.name = name;
+    this.roll= roll;
+    this.age= age;
+  }
+  getStudentDetail(){
+    console.log(`Name : ${this.name}, Roll : ${this.roll}, Age : ${this.age}`)
+  }
+}
 
-// eatSomething(function(){
-//     dinner(function(){
-//         gotobed()
-//     })
-// })
+const user1 = new Student("Rahul",3400,34)
+user1.getStudentDetail()
+const user2 = new Student("Ram",5200,25)
+user2.getStudentDetail()
