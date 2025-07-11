368de729b51ca51cc96e4832cee807d4bb1746bb
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-07-11 17:34:53 +0530

    Valid Parenthesis

---
+++ Python/validParenthesis.py
@@ -0,0 +1,26 @@
+
+balance = "([{}])"
+
+def valid(str):
+  stack = []
+  for i in range(len(str)):
+    ch = str[i]
+    if ch == "(" or ch == "{" or ch == "[":
+      stack.append(ch)
+    else:
+      if len(stack) == 0:
+        return False
+      temp = stack.pop()
+      if ch == ")" and temp != "(":
+        return False
+      if ch == "}" and temp != "{":
+        return False
+      if ch == "]" and temp != "[":
+        return False
+  return len(stack) == 0
+
+print(valid("((("))        # False
+print(valid("()[]{}"))     # True
+print(valid("([{}])"))     # True
+print(valid("([)]"))       # False
+print(valid("(((]))"))     # False
