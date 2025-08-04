260f5dd0108dd98f458a24e4c9ea3875be7415d1
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-08-04 19:00:36 +0530

    1 sec print

--- JavaScript/tempCodeRunnerFile.js
+++ JavaScript/tempCodeRunnerFile.js
@@ -1,19 +1,9 @@
-function Composed(pipeline){
-  return function(num){
-    for (let fun of pipeline){
-      num = fun(num)
-    }
-    return num
-  }
+
+
+for (let i=1; i<=10; i++){
+
+  setTimeout(()=>{
+    console.log(i)
+  },i*1000)
 }
 
-
-const pipeline = [
-  (num) => num - 1,
-  (num) => num * 10,
-  (num) => `${num} as a string`,
-];
-
-const compose = Composed(pipeline);
-
-console.log(compose(4));
