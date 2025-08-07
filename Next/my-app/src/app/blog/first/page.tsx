b9b4db1fcdb5f1f3fe8d162fa3a8e3c1ca95d7ce
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2025-08-07 19:06:31 +0530

    Nested Routing

---
+++ Next/my-app/src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+export default function FirstBlogPage(){
+
+  return (
+    <div>
+      <h1>Welcome to the First Blog Page</h1>
+    </div>
+  )
+}
---
+++ Next/my-app/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Profile(){
+
+  return (
+    <div>
+      <h1>Welcome to Profile Page</h1>
+    </div>
+  )
+}
